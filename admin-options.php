c86f1066af1b9f3c39f0c1683260b2509e737147
pressupinc <pressupinc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-31 21:50:04 +0000

    deploy from git
    git-svn-id: https://plugins.svn.wordpress.org/require-featured-image/trunk@830939 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin-options.php
@@ -0,0 +1,65 @@
+<?php
+
+add_action( 'admin_menu', 'rfi_admin_add_page' );
+function rfi_admin_add_page() {
+	add_options_page( 'Require Featured Image Page', 'Req Featured Image', 'manage_options', 'rfi', 'rfi_options_page' );
+}
+
+function rfi_options_page() {
+?>
+<div class="wrap">
+	<h2>Require Featured Image</h2>
+	<form action="options.php" method="post">
+		<?php settings_fields( 'rfi_options' ); ?>
+		<?php do_settings_sections( 'rfi' ); ?>
+		 
+		<input name="Submit" type="submit" value="<?php esc_attr_e( 'Save Changes' ); ?>" class="button button-primary" />
+	</form>
+</div>
+<?php
+}
+
+add_action( 'admin_init', 'rfi_admin_init' );
+function rfi_admin_init(){
+	// Create Settings
+	// register_setting( 'rfi_options', 'rfi_notification_text', 'rfi_text_validate' );
+	register_setting( 'rfi_options', 'rfi_post_types' );
+	
+	// Create section of Page
+	add_settings_section( 'rfi_main', 'Post Types', 'rfi_main_section_text_output', 'rfi' );
+	
+	// Add fields to that section
+	// add_settings_field( 'rfi_notification_text', 'Notification Text: ', 'rfi_notification_input_renderer', 'rfi', 'rfi_main' );
+	add_settings_field( 'rfi_post_types', 'Post Types that require featured images ', 'rfi_post_types_input_renderer', 'rfi', 'rfi_main' );
+}
+
+function rfi_main_section_text_output() {
+	echo '<p>You can specify the post type for Require Feautured Image to work on. By default it works on Posts only.</p>
+	<p>If you\'re not seeing a post type here that you think should be, it probably does not have support for featured images. Only post types that support featured images will appear on this list.</p>';
+}
+
+function rfi_notification_input_renderer() {
+	$option = get_option( 'rfi_notification_text' );
+	echo "<input id='rfi_notification_text' name='rfi_notification_text' size='60' type='text' value='{$option}' />";
+}
+
+function rfi_post_types_input_renderer() {
+	$option = rfi_return_post_types_option();
+	$post_types = get_post_types( array( 'public' => true ), 'objects' );
+
+	foreach ( $post_types as $type => $obj ) {
+		if ( ! post_type_supports( $type, 'thumbnail' ) ) {
+			continue;
+		}
+		if ( in_array( $type, $option ) ) {
+			echo '<input type="checkbox" name="rfi_post_types[]" value="'.$type.'" checked="checked">'.$obj->label.'<br>';
+		} else {
+			echo '<input type="checkbox" name="rfi_post_types[]" value="'.$type.'">'.$obj->label.'<br>';
+		}
+	}
+}
+
+function rfi_text_validate( $input ) {
+	$validated = trim( $input );
+	return $validated;
+}
--- require-featured-image.php
+++ require-featured-image.php
@@ -4,28 +4,71 @@ Plugin Name: Require Featured Image
 Plugin URI: http://pressupinc.com/wordpress-plugins/require-featured-image/
 Description: Like it says on the tin: requires posts to have a featured image set before they'll be published.
 Author: Press Up
-Version: 0.3.0
+Version: 0.5.0
 Author URI: http://pressupinc.com
 */ 
 
-add_action( 'pre_post_update', 'rfi_dont_publish' );
+require_once('admin-options.php');
 
-function rfi_dont_publish($post_ID) {
-    $post = get_post($post_ID);
-    // Incredible HACKERY because I can't find a hook that does what I want, or where
-    $request_publish_test = isset($_REQUEST['publish']);
-    $request_under_status_test = isset($_REQUEST['_status']) && $_REQUEST['_status'] == 'publish';
-    if ( $post->post_type == 'post' 
-    	&& ( $request_publish_test || $request_under_status_test ) 
-    	&& !has_post_thumbnail($post_ID) ) {
-        wp_die( 'You cannot publish a post without it having a featured image.' );
+register_activation_hook( __FILE__, 'rfi_set_default_on_activation' );
+function rfi_set_default_on_activation() {
+    add_option( 'rfi_post_types', array('post') );
+}
+
+
+add_action( 'pre_post_update', 'rfi_guard_against_publish' );
+function rfi_guard_against_publish( $post_ID ) {
+    if ( !rfi_should_let_id_publish( $post_ID ) ) {
+        wp_die( 'You cannot publish without a featured image.' );
     }
 }
 
-add_action( 'admin_enqueue_scripts', 'rfi_admin_js' );
-function rfi_admin_js($hook) {
-	if( $hook != 'post.php' && $hook != 'post-new.php' )
+
+add_action( 'admin_enqueue_scripts', 'rfi_enqueue_edit_screen_js' );
+function rfi_enqueue_edit_screen_js( $hook ) {
+
+    global $post;
+	if ( $hook != 'post.php' && $hook != 'post-new.php' )
         return;
-    wp_register_script( 'rfi-admin-js', plugins_url('/require-featured-image-on-edit.js', __FILE__), array('jquery'));
-    wp_enqueue_script( 'rfi-admin-js' );
+
+    if ( in_array( $post->post_type, rfi_return_post_types() ) ) {
+        wp_register_script( 'rfi-admin-js', plugins_url( '/require-featured-image-on-edit.js', __FILE__ ), array( 'jquery' ) );
+        wp_enqueue_script( 'rfi-admin-js' );
+    }
+}
+
+/**
+ * These are helpers that aren't ever registered with events
+ */
+
+function rfi_return_post_types() {
+    return apply_filters( 'rfi_post_types' , rfi_return_post_types_option() ); 
+}
+
+function rfi_return_post_types_option() {
+    $option = get_option( 'rfi_post_types', 'default' );
+    if ( $option === 'default' ) {
+        // Because there's no update hook, this should take care of people who hadn't set the option on activation and are used to 0.3.0 behaviour
+        $option = array( 'post' );
+    } 
+    elseif ( $option === '' ) {
+        // For people who want the plugin on, but doing nothing
+        $option = array();
+    }
+    return $option;
+}
+
+function rfi_should_let_id_publish( $post_ID ) {
+    $post = get_post( $post_ID );
+    
+    // Incredible HACKERY because I can't find a hook that does what I want, or where
+    $request_publish_test = isset($_REQUEST['publish']);
+    $request_under_status_test = isset($_REQUEST['_status']) && $_REQUEST['_status'] == 'publish';
+    $are_trying_to_publish = ( $request_publish_test || $request_under_status_test );
+
+    return !( 
+        in_array( $post->post_type, rfi_return_post_types() )
+        && $are_trying_to_publish 
+        && !has_post_thumbnail( $post_ID ) 
+    );
 }
